8d96c717eaebb7416127a4aa6229c11d80bafd84
Arístides Pérez <152738165+Aristides-19@users.noreply.github.com>
2025-03-09 22:18:54 -0400

    feat(users): add custom hooks for user management and size fetching

---
+++ src/services/users.js
@@ -0,0 +1,108 @@
+import { db } from '../firebase';
+import { doc, getDoc, collection, getCountFromServer, setDoc, query, where } from 'firebase/firestore';
+
+/**
+ * Fetch user by id.
+ * @param userIdOrPath User ID or path to the document.
+ * @returns {Promise<DocumentData>}
+ * Resolved with a user object.
+ */
+export const getUser = async (userIdOrPath) => {
+  try {
+    const userRef =
+      userIdOrPath.startsWith('users') || userIdOrPath.startsWith('/users')
+        ? doc(db, userIdOrPath)
+        : doc(db, 'users', userIdOrPath);
+
+    const userDoc = await getDoc(userRef);
+
+    if (userDoc.exists()) {
+      return userDoc.data();
+    } else {
+      console.log('Not found user by id: ', userIdOrPath);
+      return null;
+    }
+  } catch (error) {
+    console.error('Failed to fetch user by id: ' + userIdOrPath + ' : ', error);
+    throw error;
+  }
+};
+
+/**
+ * Fetch user's collection size per type
+ * @return {Promise<{students: number, guides: number}>}
+ */
+export const getUsersSize = async () => {
+  try {
+    const usersCollection = collection(db, 'users');
+
+    const qStudents = query(usersCollection, where('role', '==', 'Estudiante'));
+    const snapshotStudents = await getCountFromServer(qStudents);
+    const countStudents = snapshotStudents.data().count;
+
+    const qGuides = query(usersCollection, where('role', '==', 'Guía'));
+    const snapshotGuides = await getCountFromServer(qGuides);
+    const countGuides = snapshotGuides.data().count;
+
+    return {
+      students: countStudents,
+      guides: countGuides,
+    };
+  } catch (error) {
+    console.error('Failed to fetch users size: ', error);
+    throw error;
+  }
+};
+
+/**
+ * Save or edit a user.
+ * @param userData User data object.
+ * @returns {Promise<{email, name, bio, phone, genre, role, profilePicture, bannerPicture, excursionsHistory, forumEntries, likes}>}
+ * Resolved with a user objects.
+ */
+export const saveUser = async (userData) => {
+  const {
+    userIdOrPath,
+    email,
+    name,
+    bio,
+    phone,
+    genre,
+    role,
+    profilePicture,
+    bannerPicture,
+    excursionsHistory,
+    forumEntries,
+    likes,
+  } = userData;
+
+  try {
+    const userRef =
+      userIdOrPath.startsWith('users') || userIdOrPath.startsWith('/users')
+        ? doc(db, userIdOrPath)
+        : doc(db, 'users', userIdOrPath);
+
+    const user = {
+      email,
+      name,
+      bio,
+      phone,
+      genre,
+      role,
+      profilePicture,
+      bannerPicture,
+      excursionsHistory,
+      forumEntries,
+      likes,
+    };
+
+    // merge: true, if there is an update, it won't change the entire document, only the fields that are passed in the object
+    await setDoc(userRef, user, { merge: true });
+
+    console.log('User saved/update successfully: ', userIdOrPath);
+    return user;
+  } catch (error) {
+    console.error('Error saving/updating user ' + userIdOrPath + ' : ', error);
+    throw error;
+  }
+};
