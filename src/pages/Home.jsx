7bd375dabd67bfaeed5d85b12b27a049764c0ea9
AdrianR06 <adrian.rondon@correo.unimet.edu.ve>
2025-03-10 21:27:36 -0400

    fix: update MissionVision images and adjust paragraph margin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MissionVision from '../components/MissionVision/MissionVision';
-import image from '../assets/404-NotFound-Illustration.png';
+import imageMission from '../assets/Imagen Mision.jpg';
+import imageVision from '../assets/Imagen_Vision.jpg';
 
 function Home() {
   return (
@@ -8,12 +9,12 @@ function Home() {
       <MissionVision
         title='Misión'
         text='Buscamos reconectar a los estudiantes de la Universidad Metropolitana con el Parque Nacional El Ávila, a través de una plataforma accesible e intuitiva, con información completa y actualizada sobre rutas y actividades recreativas, en un entorno de honestidad, sustentabilidad y compromiso con la naturaleza.'
-        image={image}
+        image={imageMission}
       />
       <MissionVision
         title='Visión'
         text='Ser la plataforma digital líder en la Universidad Metropolitana que motive y conecte a los estudiantes a descubrir, cuidar y disfrutar el Parque Nacional El Ávila. Aspiramos a construir una comunidad de estudiantes activos que valoren las experiencias al aire libre y el cuidado del medio ambiente.'
-        image={image}
+        image={imageVision}
         imageRight
       />
     </>
